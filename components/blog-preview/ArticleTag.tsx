185304c7a69841f54805e5a30a75b5c27be302ec
joeynweke <97444149+joeynweke@users.noreply.github.com>
2023-03-17 18:39:06 +0100

    Update ArticleTag.tsx

--- components/blog-preview/ArticleTag.tsx
+++ components/blog-preview/ArticleTag.tsx
@@ -8,15 +8,15 @@ const tagBaseStyle = "text-xs mr-2 py-1.5 px-4 text-black-600 rounded-2xl";
 
 const textsStyles: any = {
 
-  Kitchen | Accessories: `${tagBaseStyle} bg-sky-100  text-sky-600`,
-  NodeJS: `${tagBaseStyle} bg-green-100  text-green-600`,
-  AWS: `${tagBaseStyle} bg-yellow-100  text-yellow-600`,
-  NextJS: `${tagBaseStyle}  bg-cyan-100  text-cyan-600`,
-  Docker: `${tagBaseStyle}  bg-sky-100  text-sky-600`,
-  TypeScript: `${tagBaseStyle}  bg-indigo-100  text-indigo-600`,
-  Kubernetes: `${tagBaseStyle}  bg-violet-100  text-violet-600`,
-  GCP: `${tagBaseStyle}  bg-rose-100  text-rose-600`,
-  ExpressJS: `${tagBaseStyle}  bg-lime-100  text-lime-600`,
+  Kitchen: `${tagBaseStyle} bg-sky-100  text-sky-600`,
+  Accessories: `${tagBaseStyle} bg-green-100  text-green-600`,
+  Cooking: `${tagBaseStyle} bg-yellow-100  text-yellow-600`,
+  Tools: `${tagBaseStyle}  bg-cyan-100  text-cyan-600`,
+  Culinary: `${tagBaseStyle}  bg-sky-100  text-sky-600`,
+  Utensils: `${tagBaseStyle}  bg-indigo-100  text-indigo-600`,
+  Food: `${tagBaseStyle}  bg-violet-100  text-violet-600`,
+  Baking: `${tagBaseStyle}  bg-rose-100  text-rose-600`,
+  Essentials: `${tagBaseStyle}  bg-lime-100  text-lime-600`,
 };
 export default function ArticleTag({ title }: ArticleTagProps) {
   return <div className={textsStyles[`${title}`]}>{title}</div>;
