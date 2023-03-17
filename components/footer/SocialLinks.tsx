7c548a735278a167dde9770aa6c4d0e21b0666ae
joeynweke <97444149+joeynweke@users.noreply.github.com>
2023-03-17 20:06:24 +0100

    Update SocialLinks.tsx

--- components/footer/SocialLinks.tsx
+++ components/footer/SocialLinks.tsx
@@ -5,9 +5,9 @@ const SocialLinks = () => {
   const styleClassName = "cursor-pointer mx-4 text-2xl bg-grey-50";
   return (
     <div className="flex justify-center dark:text-gray-50">
-      <FaLinkedin
+      <FaFacebook
         size={ICON_SIZE}
-        onClick={() => window.open(process.env.NEXT_PUBLIC_LINKEDIN_PROFILE_URL, "_blank")}
+        onClick={() => window.open(process.env.NEXT_PUBLIC_FACEBOOK_PROFILE_URL, "_blank")}
         className={styleClassName}
       />
       <FaYoutube
@@ -15,21 +15,13 @@ const SocialLinks = () => {
         onClick={() => window.open(process.env.NEXT_PUBLIC_YOUTUBE_CHANNEL_URL, "_blank")}
         className={styleClassName}
       />
-      <FaGithubSquare
+
+      <FaTwitter
         size={ICON_SIZE}
-        onClick={() => window.open(process.env.NEXT_PUBLIC_GITHUB_PROFILE_URL, "_blank")}
+        onClick={() => window.open(process.env.NEXT_PUBLIC_TWITTER_CHANNEL_URL, "_blank")}
         className={styleClassName}
       />
-      <FaMedium
-        size={ICON_SIZE}
-        onClick={() => window.open(process.env.NEXT_PUBLIC_MEDIUM_PROFILE_URL, "_blank")}
-        className={styleClassName}
-      />
-      <FaStackOverflow
-        size={ICON_SIZE}
-        onClick={() => window.open(process.env.NEXT_PUBLIC_STACKOVERFLOW_PROFILE_URL, "_blank")}
-        className={styleClassName}
-      />
+
     </div>
   );
 };
